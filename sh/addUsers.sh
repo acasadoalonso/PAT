965494b23c893ac04696b03c824c2fc12b745f85
acasadoalonso <acasado@acm.org>
2026-03-30 19:06:21 +0200

    cleaning some .sh files

---
+++ sh/addUsers.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# This scrept take the json files exported from on keycloak system and import it on the current kc --- it is used to sync both systems
+#
+echo
+echo "Current setup environment"
+echo
+export HOSTIP=$(hostname -I | awk '{ print $1 }' | tail -n1)
+export KEYCLOAK_URL=http://$HOSTIP:8081         # URL to call Keycloak
+echo "Keycloak URL: "$KEYCLOAK_URL
+echo "keycloak ADMIN: "$KEYCLOAK_ADMIN
+echo "keycloak ADMIN_PASSWORD: "$KEYCLOAK_ADMIN_PASSWORD
+shopt -s expand_aliases
+#alias  kcadm="docker exec keycloak bash /opt/keycloak/bin/kcadm.sh"
+#export kcadm="docker exec keycloak bash /opt/keycloak/bin/kcadm.sh"
+$kcadm config credentials --server $KEYCLOAK_URL  --realm master --user "$KEYCLOAK_ADMIN" --password "$KEYCLOAK_ADMIN_PASSWORD"
+echo
+echo "Intallation type: "$1
+echo "Importing users from json files"
+echo
+
+cd jsonfiles
+for f in $(sudo ls  cpas*.json); do
+        rm -f tmpuser.json
+        echo "Processing file:" $f
+	echo '{' >cbo && tail -n +4 $f | head -n-2  > tmp.json && echo '}' >cbc && cat cbo tmp.json cbc >tmpuser.json
+        if [[ $1 != 'bash' ]] 
+        then
+	   docker cp ./tmpuser.json keycloak:/var/tmpuser.json
+	   $kcadm create users    -r cpas -f /var/tmpuser.json
+        else
+	   $kcadm create users    -r cpas -f ./tmpuser.json
+	fi
+	rm tmpuser.json cbo tmp.json cbc
+	echo "User: "$(jq -r '.users[0].username' $f)
+        echo
+	$kcadm add-roles --uusername $(jq -r '.users[0].username' $f) --rolename user               -r cpas
+	$kcadm add-roles --uusername $(jq -r '.users[0].username' $f) --rolename default-roles-cpas -r cpas
+        role1=$(jq -r '.users[0].realmRoles[0]' $f) 
+	role2=$(jq -r '.users[0].realmRoles[1]' $f)  
+	role3=$(jq -r '.users[0].realmRoles[2]' $f)  
+        role=$(jq -r '.users[0].realmRoles[0]' $f)
+        x=1 
+        while [[ $role != 'null' ]] ; 
+        do
+          if [[ $role == 'default-roles-cpas' ||  $role  == 'user' ]]
+          then
+            echo 'std roles ...: '$role
+          else
+            echo "Added role:    "$role
+	    $kcadm add-roles --uusername $(jq -r '.users[0].username' $f) --rolename "$role" -r cpas
+          fi
+          role=$(jq -r '.users[0].realmRoles['$x']' $f)
+          #echo "Role: "$role
+          x=$(( $x + 1 ))
+        done
+done
+
--- sh/instasDocker.sh
+++ sh/instasDocker.sh
@@ -26,14 +26,41 @@ echo "============================"
 echo
 # check for curl and install it if needed
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+#
+######################################################
+sudo apt update
+sudo apt upgrade -y
+mkdir -p ~/src
+mkdir -p ~/src/pat
+mkdir -p ~/src/sh
+echo
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+sudo mkdir -p /etc/apt/keyrings
+# install gh
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+# install the service programs
+echo 
+echo "Install service programs"
+echo "========================"
+echo 
+sudo apt-get install -y wget systemd git libarchive-dev  vim inetutils-ping figlet ntpdate ssh sudo openssh-server
+sudo apt-get install -y gh jq gcc g++ make curl neofetch iproute2 ca-certificates gnupg libfmt-dev logrotate net-tools
+#
+######################################################
+
 echo
 echo "Install the PAT software from GitHub"
 echo
 echo "Current dir: "$(pwd)
 echo "=================================="
 echo
-rm -rf patServer
-rm -rf patClient
+cd ~/src/pat
+sudo rm -rf patServer
+sudo rm -rf patClient
 echo "Login into github ..."
 echo "====================="
 gh auth login --with-token < ../../mytoken.txt
@@ -62,14 +89,10 @@ echo
 echo 
 echo "Setup the aliases ..."
 echo 
-alias pat='(cd ~/src/PAT/dockerfiles/patServer && docker compose up -d)'
-alias patrestart='(cd ~/src/PAT/dockerfiles/patServer && docker compose restart) '
 alias kcadm="docker exec keycloak bash /opt/keycloak/bin/kcadm.sh"
 
 echo
-echo "alias pat='(cd ~/src/PAT/dockerfiles/patServer && docker compose up)'"                         >>~/.bash_aliases
-echo "alias patrestart='(cd ~/src/PAT/dockerfiles/patServer && docker compose restart)'"             >>~/.bash_aliases
-echo "alias status='(pgrep -a node;echo;pgrep -a java;echo;sudo netstat -ano -p tcp|grep 8080;echo)'" >>~/.bash_aliases
+echo "alias status='(pgrep -a node;echo;pgrep -a java;echo;sudo netstat -ano -p tcp|grep 8000;echo)'" >>~/.bash_aliases
 echo "alias kcadm='docker exec keycloak bash /opt/keycloak/bin/kcadm.sh'"                            >>~/.bash_aliases
 
 ###########################################################################################################
@@ -85,6 +108,7 @@ if [[ $CONTAINERIP == '' ]] ; then
 fi
 #
 # setup the Keycloak realm
+
 #
 echo "============================"
 echo "Container IP:  "$CONTAINERIP
